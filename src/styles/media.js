c0cb097c3e4a1e331236b291e2c7a2f115c7c4a7
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-25 00:02:04 +0900

    Add : Flex Set, Position Set, Media Query Set

---
+++ src/styles/media.js
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+
+const breakpoint = {
+  desktop: 1080,
+};
+
+export default Object.keys(breakpoint).reduce((acc, device) => {
+  acc[device] = (...attribute) => css`
+    @media screen and (min-width: ${breakpoint[device]}px) {
+      ${css(...attribute)};
+    }
+  `;
+  return acc;
+}, {});
