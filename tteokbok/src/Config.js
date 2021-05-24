2958d11e67e8616c355c153460ee6f3fff81ef6b
sunkyungnoh <cellin1637@gmail.com>
2021-05-24 20:58:44 +0900

    DELETE: Undo Initial Settings

--- tteokbok/src/Config.js
+++
@@ -1,2 +0,0 @@
-const IP = '';
-export const API = {};
--- westagram-react/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "./Routes";
-import "./styles/common.scss";
-
-ReactDOM.render(<Routes />, document.getElementById("root"));
